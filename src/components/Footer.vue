d0707265f5686ad3bba5873e84fbde20edfb737c
yassin fhm <yassin.fahe12@gmail.com>
2025-12-17 14:54:54 +0100

    Update cv v2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -40,7 +40,7 @@ const year = new Date().getFullYear()
       </a>
 
       <a
-        href="/CV_Yassin_Fahem.pdf" 
+        href="`${import.meta.env.BASE_URL}CV_Yassin_Fahem.pdf`" 
         target="_blank"
         rel="noopener noreferrer"
         class="cv-link"
