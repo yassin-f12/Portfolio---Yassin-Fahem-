a28b99fe0ff1fb05e5db2604d8e4483fc001e94e
yassin fhm <yassin.fahe12@gmail.com>
2026-04-19 13:18:09 +0200

    fix: remove visible class removal on scroll out in useScrollAnimation

--- src/composables/useScrollAnimation.js
+++ src/composables/useScrollAnimation.js
@@ -9,14 +9,13 @@ export function useScrollAnimation() {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             entry.target.classList.add('visible')
-          } else {
-            entry.target.classList.remove('visible')
-          }
+            observer.unobserve(entry.target)
+          } 
         })
       },
       { 
         threshold: 0.2,
-        rootMargin: '-50px 0px -50px 0px'
+        rootMargin: '0px 0px -50px 0px'
       }
     )
 
